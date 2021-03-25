f82c3e550f681d47bf3e2c0c6f4e784caeeb002f
johnny-rob <jpinney1@gmail.com>
2021-03-25 08:55:55 -0400

    Resolved jquery, actiontext, and other application.js errors, but without fixing the functionality of the js bootstrap feature for expanding hidden text

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,3 @@
-
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 require("@rails/ujs").start()
 require("packs/html.sortable")
 require("turbolinks").start()
@@ -13,7 +7,10 @@ require("channels")
 require("@nathanvda/cocoon")
 require("gritter/js/jquery.gritter.js")
 
-//= require jquery
+
+//= require_tree
+
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -25,5 +22,3 @@ require("gritter/js/jquery.gritter.js")
 import "controllers"
 
 require('lib/following');
-require('trix');
-require('@rails/actiontext');
