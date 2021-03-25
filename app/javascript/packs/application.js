902a00db6bbb430960bb2c066ae67ebb05349caf
johnny-rob <jpinney1@gmail.com>
2021-03-25 08:16:50 -0400

    Updated about me page to have collapsable components, does not function due to jquery not loading

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,4 @@
+
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -5,7 +6,6 @@
 
 require("@rails/ujs").start()
 require("packs/html.sortable")
-require("cable") //This may not be necessary with rails 6
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("jquery")
@@ -13,6 +13,7 @@ require("channels")
 require("@nathanvda/cocoon")
 require("gritter/js/jquery.gritter.js")
 
+//= require jquery
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,8 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+}))
 
 module.exports = environment
-
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
-  })
-)
